951e97669c22574555934d5e7e4b6b9a496c5966
Dmitrii Bledniuc <ingoodshape93@gmail.com>
2020-04-28 18:50:51 +0300

    all done except 3

---
+++ factorial/script.js
@@ -0,0 +1,7 @@
+let result = 1;
+let number = 5;
+while(number>=2) {
+  result*=number;
+  --number
+  console.log(result)
+}
---
+++ script.js
@@ -0,0 +1,9 @@
+for(let i=0; i<=10; i++) {
+    if (i%2) {
+      continue;
+    } else if (i==0) {
+      continue;
+    } else {
+      console.log(i);
+    }
+  }
